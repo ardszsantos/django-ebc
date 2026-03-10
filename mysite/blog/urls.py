de9931699d5417ceb7c7c2a2e71fb4a54d52cbc2
ale r <lokgamer01@gmail.com>
2026-03-10 11:40:54 -0300

    ..

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.post, name='home'),
+    path('', views.post_list, name='home'),
+    path('post/create/', views.post_create, name='post_create'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.text import slugify
+from blog.models import Post
 
 
-def post(request):
-    return HttpResponse("Hello World")
+def post_list(request):
+    posts = Post.objects.filter(status=1)
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status=1)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title', '').strip()
+        content = request.POST.get('content', '').strip()
+        status = 1
+
+        if title and content:
+            slug = slugify(title)
+            post = Post.objects.create(
+                title=title,
+                slug=slug,
+                author=request.user,
+                content=content,
+                status=status,
+            )
+            return redirect('post_detail', slug=post.slug)
+
+    return render(request, 'blog/post_create.html')
